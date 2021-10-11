cf5d5e714165841cadd51dd064121d4bc3c852e0
Henry Lee <henryleeworld@gmail.com>
2021-10-12 00:01:15 +0800

    feat:更新最新資料

---
+++ database/migrations/2021_10_11_084423_create_taiwan_movie_theaters_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTaiwanMovieTheatersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('taiwan_movie_theaters', function (Blueprint $table) {
+            $table->string('city', 3);
+            $table->string('business_name', 24);
+            $table->string('company_name', 20);
+            $table->string('national_id_no', 8);
+            $table->smallInteger('total_rooms');
+            $table->string('address', 40);
+            $table->string('telephone', 12);
+            $table->string('remark', 50)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('taiwan_movie_theaters');
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,6 +22,7 @@ class DatabaseSeeder extends Seeder
             WorldCitiesTableSeeder::class,
             TaiwanHolidaysTableSeeder::class,
             TaiwanZipCodesTableSeeder::class,
+            TaiwanMovieTheatersTableSeeder::class,
         ]);
     }
 }
