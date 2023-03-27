a2bb60db2144e239554a789b770055090896c03d
Henry Lee <henryleeworld@gmail.com>
2023-03-28 00:05:26 +0800

    feat:升級至 laravel 9

--- database/migrations/2021_04_26_072233_create_s_and_p_500_companies_table.php
+++ database/migrations/2021_04_26_072233_create_s_and_p_500_companies_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateSAndP500CompaniesTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -40,4 +40,4 @@ class CreateSAndP500CompaniesTable extends Migration
     {
         Schema::dropIfExists('s_and_p_500_companies');
     }
-}
+};
