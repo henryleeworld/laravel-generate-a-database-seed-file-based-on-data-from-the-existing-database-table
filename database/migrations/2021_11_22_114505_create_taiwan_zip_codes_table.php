cd0463467b5927f2c313111992e32b47fa978bed
Henry Lee <henryleeworld@gmail.com>
2023-04-06 00:08:58 +0800

    feat:更新最新資料

--- database/migrations/2021_11_22_114505_create_taiwan_zip_codes_table.php
+++ database/migrations/2021_11_22_114505_create_taiwan_zip_codes_table.php
@@ -14,11 +14,12 @@ return new class extends Migration
     public function up()
     {
         Schema::create('taiwan_zip_codes', function (Blueprint $table) {
-            $table->integer('zip_code');
-            $table->string('city', 3);
-            $table->string('area', 4);
-            $table->string('road', 15);
-            $table->string('scope', 23);
+            $table->increments('id')->comment('編號');
+            $table->integer('zip_code')->comment('郵遞區號');
+            $table->string('city', 3)->comment('縣市名稱');
+            $table->string('area', 4)->comment('鄉鎮市區');
+            $table->string('road', 15)->comment('原始路名');
+            $table->string('scope', 23)->comment('投遞範圍');
         });
     }
 
