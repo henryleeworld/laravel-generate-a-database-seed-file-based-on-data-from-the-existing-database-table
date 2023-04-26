f5e52c5054f5879c84a3f612ee631b6e07e0f0a6
Henry Lee <henryleeworld@gmail.com>
2023-04-27 00:08:47 +0800

    feat:更新最新資料

---
+++ database/migrations/2023_04_13_163432_create_airlines_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('airlines', function (Blueprint $table) {
+            $table->integer('id', true)->comment('編碼');
+            $table->string('iata_code', 2)->nullable()->comment('國際航空代碼');
+            $table->string('icao_code', 3)->nullable()->comment('國際民航組織代碼');
+            $table->string('name_in_en', 32)->nullable()->comment('英文名稱');
+            $table->string('alias_name_in_en', 32)->nullable()->comment('英文別名');
+            $table->string('name_in_zh_tw', 20)->nullable()->comment('繁體中文名稱');
+            $table->string('alias_name_in_zh_tw', 6)->nullable()->comment('繁體中文別名');
+            $table->string('address', 48)->nullable()->comment('地址');
+            $table->string('telephone', 12)->nullable()->comment('電話');
+            $table->string('nationality', 2)->nullable()->comment('國籍');
+            $table->string('updated_at', 25)->nullable()->comment('更新時間');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('airlines');
+    }
+};
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             AirportsTableSeeder::class,
+            AirlinesTableSeeder::class,
             CountriesTableSeeder::class,
             CurrenciesTableSeeder::class,
             NasdaqListedCompaniesTableSeeder::class,
