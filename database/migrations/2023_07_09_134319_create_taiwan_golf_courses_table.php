2e5f022e5d4a99a83e200e5d7437e6aa880c7faf
Henry Lee <henryleeworld@gmail.com>
2023-08-14 00:09:19 +0800

    feat:更新最新資料

---
+++ database/migrations/2023_07_09_134319_create_taiwan_golf_courses_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('taiwan_golf_courses', function (Blueprint $table) {
+            $table->integer('id')->primary()->comment('編號');
+            $table->string('name', 16)->comment('名稱');
+            $table->string('city', 3)->comment('所在縣市');
+            $table->string('address', 21)->comment('所在地');
+            $table->integer('holes')->comment('洞數');
+            $table->decimal('hectares', 10, 6)->comment('面積公頃');
+            $table->string('document_number', 20)->comment('開放使用文號');
+            $table->string('business_entity', 15)->comment('經營主體');
+            $table->string('telephone', 14)->comment('連絡電話');
+            $table->string('fax', 12)->comment('傳真');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('taiwan_golf_courses');
+    }
+};
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -38,6 +38,7 @@ class DatabaseSeeder extends Seeder
             TaiwanBuskersTableSeeder::class,
             TaiwanVegetarianRestaurantsTableSeeder::class,
             TaiwanFuneralFacilitiesTableSeeder::class,
+            TaiwanGolfCoursesTableSeeder::class,
         ]);
     }
 }
