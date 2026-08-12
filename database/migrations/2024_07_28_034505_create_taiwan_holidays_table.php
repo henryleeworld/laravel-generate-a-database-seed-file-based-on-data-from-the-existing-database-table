49cf497214dd06926bbcf2e0b21b8164c3888e3b
Henry Lee <henryleeworld@gmail.com>
2026-08-13 00:18:31 +0800

    feat:更新最新資料

--- database/migrations/2024_07_28_034505_create_taiwan_holidays_table.php
+++ database/migrations/2024_07_28_034505_create_taiwan_holidays_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('taiwan_holidays', function (Blueprint $table) {
             $table->date('date')->primary()->comment('日期');
             $table->integer('year')->comment('西元年');
-            $table->string('name', 14)->nullable()->comment('節日或紀念日名稱');
+            $table->string('name', 15)->nullable()->comment('節日或紀念日名稱');
             $table->string('is_holiday', 1)->comment('是否放假');
             $table->string('holiday_category', 9)->comment('放假類別');
             $table->string('description', 74)->nullable()->comment('說明');
